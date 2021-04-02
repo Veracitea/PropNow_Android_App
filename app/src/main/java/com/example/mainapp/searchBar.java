5e1f2a02cb7883738cad8d7ff1de7dad74ccde8a
kirubhaharini <kirubhah@gmail.com>
2021-04-02 19:23:54 +0800

    changed searchbar back button

--- app/src/main/java/com/example/mainapp/searchBar.java
+++ app/src/main/java/com/example/mainapp/searchBar.java
@@ -6,18 +6,27 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class searchBar extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+
     //changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_bar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //close drawer
+        MainActivity.closeDrawer(drawerLayout);
     }
 
-    public void ClickBackBtn(View view){
-        redirectActivity(searchBar.this, MainActivity.class);
-    }
+
+
 
     public static void redirectActivity(Activity activity, Class aClass) {
         //Initialize intent
@@ -26,6 +35,49 @@ public class searchBar extends AppCompatActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         //start activity
         activity.startActivity(intent);
+    }
+
+    //SIDEBAR OPTIONS - FUNCTIONS
+    public void ClickMenu(View view){ MainActivity.openDrawer(drawerLayout); }
+
+    //MAIN MENU
+    public void ClickHome(View view){ MainActivity.redirectActivity(this,MainActivity.class); }
+    public void ClickAdvFilters(View view){ recreate(); }
+
+    //VIEW GRANT INFO
+    public void ClickViewGrantsInfo(View view){ MainActivity.redirectActivity(this,ViewGrantsInfo.class); }
+
+    //VIEW AGENT INFO
+    public void ClickViewAgentInfo(View view){ MainActivity.redirectActivity(this,ViewAgentInfo.class); }
+    public void ClickAgent(View view){
+        MainActivity.redirectActivity(this,MonicaGeller.class);
+    }
+
+    //HOME CALC
+    public void ClickHomeCalculator(View view){ MainActivity.redirectActivity(this,HomeCalculator.class); }
+
+    //MY LISTINGS
+    public void ClickMyListings(View view){
+        MainActivity.redirectActivity(this,MyListings.class);
+    }
+
+    //INBOX
+    public void ClickInbox(View view){
+        MainActivity.redirectActivity(this,Inbox.class);
+    }
+    public void ClickEditInbox(View view){
+        MainActivity.redirectActivity(this,EditInbox.class);
+    }
+
+    //SETTINGS
+    public void ClickSettings(View view){
+        MainActivity.redirectActivity(this,Settings.class);
+    }
+
+
+
+    public void ClickBackBtn2(View view){
+        MainActivity.redirectActivity(this, MainActivity.class);
     }
 
 }
