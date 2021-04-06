a3b2f671027c41dda984ec139c1112a318b0dd0f
kathy060415 <kathy060415@gmail.com>
2021-04-07 01:12:20 +0800

    house info

--- app/src/main/java/com/example/mainapp/houseInfo.java
+++ app/src/main/java/com/example/mainapp/houseInfo.java
@@ -6,24 +6,65 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class houseInfo extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_house_info);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
     }
     public void ClickBackBtn3(View view) {
-        redirectActivity(this, MainActivity.class);
+        MainActivity.redirectActivity(this, MainActivity.class);
     }
 
-    public static void redirectActivity(Activity activity, Class aClass) {
-        //Initialize intent
-        Intent intent = new Intent(activity,aClass);
-        //set flag
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        //start activity
-        activity.startActivity(intent);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //close drawer
+        MainActivity.closeDrawer(drawerLayout);
+    }
+
+    //SIDEBAR OPTIONS - FUNCTIONS
+    public void ClickMenu(View view){ MainActivity.openDrawer(drawerLayout); }
+
+    //MAIN MENU
+    public void ClickHome(View view){ MainActivity.redirectActivity(this,MainActivity.class); }
+    public void ClickAdvFilters(View view){ recreate(); }
+
+    //VIEW GRANT INFO
+    public void ClickViewGrantsInfo(View view){ MainActivity.redirectActivity(this,ViewGrantsInfo.class); }
+
+    //VIEW AGENT INFO
+    public void ClickViewAgentInfo(View view){ MainActivity.redirectActivity(this,ViewAgentInfo.class); }
+    public void ClickAgent(View view){
+        MainActivity.redirectActivity(this,MonicaGeller.class);
+    }
+
+    //HOME CALC
+    public void ClickHomeCalculator(View view){ MainActivity.redirectActivity(this,HomeCalculator.class); }
+
+    //MY LISTINGS
+    public void ClickMyListings(View view){
+        MainActivity.redirectActivity(this,MyListings.class);
+    }
+
+    //INBOX
+    public void ClickInbox(View view){
+        MainActivity.redirectActivity(this,Inbox.class);
+    }
+    public void ClickEditInbox(View view){
+        MainActivity.redirectActivity(this,EditInbox.class);
+    }
+
+    //SETTINGS
+    public void ClickSettings(View view){
+        MainActivity.redirectActivity(this,Settings.class);
     }
 }
+
