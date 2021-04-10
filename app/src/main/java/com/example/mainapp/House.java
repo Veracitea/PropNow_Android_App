64cbc4bd7efe71fe539b34de299a2de7dddd99f0
vaishnavi <inbanathanvaishnavi@gmail.com>
2021-04-10 19:21:32 +0800

    updated house House.java

--- app/src/main/java/com/example/mainapp/House.java
+++ app/src/main/java/com/example/mainapp/House.java
@@ -1,35 +1,63 @@
 package com.example.mainapp;
 
 public class House {
+
     private int id;
-    private int noOfRooms;
-    private String blk;
-    private String unitNo;
-    private String nearestMRT;
+    private int month;
+    private String town;
+    private String flat_type;
+    private String block;
+    private String street_name;
+    private String storey_range;
+    private String floor_area_sqm;
+    private String flat_model;
+    private String lease_commence_date;
+    private String remaining_lease;
+    private String resale_price;
+    private String agent_name;
 
     //constructors
 
 
-    public House(int id, int noOfRooms, String blk, String unitNo, String nearestMRT) {
+    public House(int id, int month, String town, String flat_type, String block, String street_name,
+                 String storey_range, String floor_area_sqm, String flat_model, String lease_commence_date,
+                 String remaining_lease, String resale_price, String agent_name) {
         this.id = id;
-        this.noOfRooms = noOfRooms;
-        this.blk = blk;
-        this.unitNo = unitNo;
-        this.nearestMRT = nearestMRT;
+        this.month = month;
+        this.town = town;
+        this.flat_type = flat_type;
+        this.block = block;
+        this.street_name = street_name;
+        this.storey_range = storey_range;
+        this.floor_area_sqm = floor_area_sqm;
+        this.flat_model = flat_model;
+        this.lease_commence_date = lease_commence_date;
+        this.remaining_lease = remaining_lease;
+        this.resale_price = resale_price;
+        this.agent_name = agent_name;
     }
 
     @Override
     public String toString() {
-        return "house{" +
+        return "House{" +
                 "id=" + id +
-                ", noOfRooms=" + noOfRooms +
-                ", blk='" + blk + '\'' +
-                ", unitNo='" + unitNo + '\'' +
-                ", nearestMRT='" + nearestMRT + '\'' +
+                ", month=" + month +
+                ", town='" + town + '\'' +
+                ", flat_type='" + flat_type + '\'' +
+                ", block='" + block + '\'' +
+                ", street_name='" + street_name + '\'' +
+                ", storey_range='" + storey_range + '\'' +
+                ", floor_area_sqm='" + floor_area_sqm + '\'' +
+                ", flat_model='" + flat_model + '\'' +
+                ", lease_commence_date='" + lease_commence_date + '\'' +
+                ", remaining_lease='" + remaining_lease + '\'' +
+                ", resale_price='" + resale_price + '\'' +
+                ", agent_name='" + agent_name + '\'' +
                 '}';
     }
 
     //getters and setters
+
 
     public int getId() {
         return id;
@@ -39,35 +67,99 @@ public class House {
         this.id = id;
     }
 
-    public int getNoOfRooms() {
-        return noOfRooms;
+    public int getMonth() {
+        return month;
     }
 
-    public void setNoOfRooms(int noOfRooms) {
-        this.noOfRooms = noOfRooms;
+    public void setMonth(int month) {
+        this.month = month;
     }
 
-    public String getBlk() {
-        return blk;
+    public String getTown() {
+        return town;
     }
 
-    public void setBlk(String blk) {
-        this.blk = blk;
+    public void setTown(String town) {
+        this.town = town;
     }
 
-    public String getUnitNo() {
-        return unitNo;
+    public String getFlat_type() {
+        return flat_type;
     }
 
-    public void setUnitNo(String unitNo) {
-        this.unitNo = unitNo;
+    public void setFlat_type(String flat_type) {
+        this.flat_type = flat_type;
     }
 
-    public String getNearestMRT() {
-        return nearestMRT;
+    public String getBlock() {
+        return block;
     }
 
-    public void setNearestMRT(String nearestMRT) {
-        this.nearestMRT = nearestMRT;
+    public void setBlock(String block) {
+        this.block = block;
+    }
+
+    public String getStreet_name() {
+        return street_name;
+    }
+
+    public void setStreet_name(String street_name) {
+        this.street_name = street_name;
+    }
+
+    public String getStorey_range() {
+        return storey_range;
+    }
+
+    public void setStorey_range(String storey_range) {
+        this.storey_range = storey_range;
+    }
+
+    public String getFloor_area_sqm() {
+        return floor_area_sqm;
+    }
+
+    public void setFloor_area_sqm(String floor_area_sqm) {
+        this.floor_area_sqm = floor_area_sqm;
+    }
+
+    public String getFlat_model() {
+        return flat_model;
+    }
+
+    public void setFlat_model(String flat_model) {
+        this.flat_model = flat_model;
+    }
+
+    public String getLease_commence_date() {
+        return lease_commence_date;
+    }
+
+    public void setLease_commence_date(String lease_commence_date) {
+        this.lease_commence_date = lease_commence_date;
+    }
+
+    public String getRemaining_lease() {
+        return remaining_lease;
+    }
+
+    public void setRemaining_lease(String remaining_lease) {
+        this.remaining_lease = remaining_lease;
+    }
+
+    public String getResale_price() {
+        return resale_price;
+    }
+
+    public void setResale_price(String resale_price) {
+        this.resale_price = resale_price;
+    }
+
+    public String getAgent_name() {
+        return agent_name;
+    }
+
+    public void setAgent_name(String agent_name) {
+        this.agent_name = agent_name;
     }
 }
--- app/src/main/java/com/example/mainapp/HouseDatabaseHelper.java
+++ app/src/main/java/com/example/mainapp/HouseDatabaseHelper.java
@@ -9,6 +9,19 @@ import androidx.annotation.Nullable;
 public class HouseDatabaseHelper extends SQLiteOpenHelper {
 
     public static final String HOUSE_TABLE = "HOUSE_TABLE";
+    public static final String COL_ID = "id";
+    public static final String COL_MONTH = "month";
+    public static final String COL_TOWN = "town";
+    public static final String COL_FLAT_TYPE = "flat_type";
+    public static final String COL_BLOCK = "block";
+    public static final String COL_STREET_NAME = "street_name";
+    public static final String COL_STOREY_RANGE = "storey_range";
+    public static final String COL_FLOOR_AREA_SQM = "floor_area_sqm";
+    public static final String COL_FLAT_MODEL = "flat_model";
+    public static final String COL_LEASE_COMMENCE_DATE = "lease_commence_date";
+    public static final String COL_REMAINING_LEASE = "remaining_lease";
+    public static final String COL_RESALE_PRICE = "resale_price";
+    public static final String COL_AGENT_NAME = "agent_name";
 
     public HouseDatabaseHelper(@Nullable Context context) {
         super(context, "House.db", null, 1);
@@ -18,7 +31,11 @@ public class HouseDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        String createTableStatement = "CREATE TABLE " + HOUSE_TABLE + "()";
+        String createTableStatement = "CREATE TABLE " + HOUSE_TABLE + "(" + COL_ID + " INT  NOT NULL PRIMARY KEY AUTO_INCREMENT, " + COL_MONTH + " VARCHAR(7) NOT NULL," +
+                COL_TOWN + " VARCHAR(15) NOT NULL , " + COL_FLAT_TYPE + " VARCHAR(16) NOT NULL, " + COL_BLOCK + " VARCHAR(4) NOT NULL, " + COL_STREET_NAME + " VARCHAR(20) NOT NULL," +
+                COL_STOREY_RANGE + " VARCHAR(8) NOT NULL, " + COL_FLOOR_AREA_SQM + " NUMERIC(4,1) NOT NULL, " + COL_FLAT_MODEL + " VARCHAR(22) NOT NULL, " +
+                COL_LEASE_COMMENCE_DATE + " INTEGER  NOT NULL, " + COL_REMAINING_LEASE + " VARCHAR(18) NOT NULL, " + COL_RESALE_PRICE + " NUMERIC(9,2) NOT NULL," +
+                COL_AGENT_NAME + " VARCHAR(20) NOT NULL)";
 
 
                /* CREATE TABLE HOUSE_TABLE (id INT  NOT NULL PRIMARY KEY AUTO_INCREMENT ,month VARCHAR(7) NOT NULL
