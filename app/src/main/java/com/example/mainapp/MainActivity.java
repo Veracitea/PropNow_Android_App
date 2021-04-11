ed1051491f311b756472e15f7ea32d2bd5d7eeff
kirubhaharini <kirubhah@gmail.com>
2021-04-11 17:22:23 +0800

    name changed

--- app/src/main/java/com/example/mainapp/MainActivity.java
+++ app/src/main/java/com/example/mainapp/MainActivity.java
@@ -116,12 +116,12 @@ public class MainActivity extends AppCompatActivity {
             viewgrants.setVisibility(View.GONE);
             homecalc.setVisibility(View.GONE);
             viewagentinfo.setVisibility(View.GONE);
-            username.setText("Monica Geller\nAgent ID: U273849K");
+            username.setText("Francisca Grand");
             picture1.setVisibility(VISIBLE);
             picture.setVisibility(View.GONE);
         } else if (domain == "NON-AGENT") {  //for non-agents
             mylistings.setVisibility(View.GONE);
-            username.setText("Rachel Green");
+            username.setText("Ealasaid MacCarrane");
             picture2.setVisibility(VISIBLE);
             picture.setVisibility(View.GONE);
         } else {  //for general users
